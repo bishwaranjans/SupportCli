806fba8c7b4629752fb5b7bccad44956a0129bdd
Bishwaranjan Sandhu <bsa@byggtjeneste.no>
2022-01-02 09:13:01 +0100

    Add project files.

---
+++ SupportCli/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SupportCli
+{
+    class Program
+    {
+        static void Main()
+        {
+            new TicketProcessor().Start();
+        }
+    }
+
+}
---
+++ SupportCli/Ticket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SupportCli
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public State CurrentState { get; set; }
+        public string AssignedToUser { get; set; }
+        public int CommentsCount { get; set; }
+        public List<string> Comments { get; set; }
+
+        public enum State
+        {
+            Open,
+            InProgress,
+            Closed
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"{nameof(Id)}={Id}");
+            Console.WriteLine($"{nameof(Title)}={Title}");
+            Console.WriteLine($"{nameof(CurrentState)}={CurrentState}");
+            Console.WriteLine($"{nameof(AssignedToUser)}={AssignedToUser}");
+            Console.WriteLine($"{nameof(CommentsCount)}={CommentsCount}");
+            Console.WriteLine($"{nameof(Comments)}:");
+            foreach (var comment in Comments)
+            {
+                Console.WriteLine($"> {comment}");
+            }
+        }
+    }
+}
